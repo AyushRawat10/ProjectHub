574ff5ea3e4149c5aa59ae43a7427b40b9db6ae5
Ayush Rawat <rawatayushh10@gmail.com>
2026-09-18 11:46:56 +0530

    add project retrieval functionality with controllers and routes

--- server/src/projects/project.controller.ts
+++ server/src/projects/project.controller.ts
@@ -26,3 +26,56 @@ export const createProjectController = async (req: Request, res: Response) => {
     })
 }
 
+export const getMyAllProjectController = async (req: Request, res: Response) => {
+    if(!req.userId) {
+        return res.status(401).json({
+            message: "Authentication required",
+        })
+    }
+
+    const result = await pool.query(
+        `
+            SELECT id, owner_id, name, description, created_at, updated_at
+            FROM projects
+            WHERE owner_id = $1
+            ORDER BY created_at DESC
+        `,
+        [req.userId]
+    )
+
+    return res.status(200).json({
+        message: "Here is your all projects",
+        projects: result.rows,
+    })
+}
+
+export const getProjectById = async (req: Request, res: Response) => {
+    if(!req.userId) {
+        return res.status(401).json({
+            message: "Authentication required",
+        })
+    }
+
+    const {id} = req.params;
+
+    const result = await pool.query(
+        `
+            SELECT id, owner_id, name, description, created_at, updated_at
+            FROM projects
+            WHERE id = $1
+                AND owner_id = $2
+        `,
+        [id, req.userId]
+    )
+
+    if(result.rows.length === 0) {
+        return res.status(404).json({
+            message: "Project not found",
+        });
+    }
+
+    return res.status(200).json({
+        message: "Succesfully found",
+        project: result.rows[0],
+    })
+}
--- server/src/projects/project.routes.ts
+++ server/src/projects/project.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { authenticate } from "../auth/auth.middleware.js";
-import { createProjectController } from "./project.controller.js";
+import { createProjectController, getMyAllProjectController, getProjectById } from "./project.controller.js";
 
 const router = Router();
 
 router.post("/", authenticate, createProjectController);
+router.get("/", authenticate, getMyAllProjectController);
+router.get("/:id", authenticate, getProjectById);
 
 export default router;
