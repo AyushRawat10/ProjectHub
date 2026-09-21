9883ddafd6c874089e318db80324094e9e2b14d2
Ayush Rawat <rawatayushh10@gmail.com>
2026-09-21 20:36:31 +0530

    add functionality to retrieve a task by ID with controller and routing

--- server/src/tasks/task.controller.ts
+++ server/src/tasks/task.controller.ts
@@ -175,3 +175,74 @@ export const getProjectTasksController = async (req: Request, res: Response) => 
 		tasks: result.rows,
 	});
 };
+
+export const getTaskByIdController = async (req: Request, res: Response) => {
+    if(!req.userId) {
+        return res.status(401).json({
+            message: "Authentication required"
+        })
+    }
+
+    const {id: projectId, taskId} = req.params;
+
+    const accessResult = await pool.query(
+        `
+            SELECT 1 
+            FROM projects
+            WHERE id = $1
+                AND owner_id = $2
+
+            UNION 
+
+            SELECT 1 
+            FROM project_members
+            WHERE project_id = $1
+                AND user_id = $2
+        `,
+        [projectId, req.userId]
+    )
+
+    if(accessResult.rows.length === 0) {
+        return res.status(404).json({
+            message: "Project not found"
+        })
+    }
+
+    const result = await pool.query(
+        `
+            SELECT 
+                t.id,
+                t.project_id,
+                t.creator_id,
+                t.assignee_id,
+                t.title,
+                t.description,
+                t.status,
+                t.priority,
+                t.due_date,
+                t.created_at,
+                t.updated_at,
+                creator.name AS creator_name,
+                assignee.name AS assignee_name
+            FROM tasks t
+            JOIN users creator
+                ON creator.id = t.creator_id
+            LEFT JOIN users assignee
+                ON assignee.id = t.assignee_id
+            WHERE t.id = $1
+                AND t.project_id = $2
+        `,
+        [taskId, projectId]
+    )
+
+    if(result.rows.length === 0) {
+        return res.status(404).json({
+            message: "Task not found"
+        })
+    }
+
+    return res.status(200).json({
+        message: "Task retrieved successfully",
+        task: result.rows[0]
+    })
+}
--- server/src/tasks/task.routes.ts
+++ server/src/tasks/task.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authenticate } from "../auth/auth.middleware.js";
-import { createTaskController, getProjectTasksController,  } from "./task.controller.js";
+import { createTaskController, getProjectTasksController, getTaskByIdController,  } from "./task.controller.js";
 
 const router = Router();
 
 router.post("/:id/tasks", authenticate, createTaskController);
 router.get("/:id/tasks", authenticate, getProjectTasksController);
+router.get("/:id/tasks/:taskId", authenticate, getTaskByIdController);
 
 export default router;
